773b590b0892331d4fc457082f5afcb7a8028646
root <root@BrettsPC.localdomain>
2025-01-27 20:56:05 -0500

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,29 @@
-import { useState } from 'react'
+import './index.css';
+import { Routes, Route, Link } from "react-router-dom";
+import Blue from './Blue.jsx';  // Ensure this import points to the correct file location
+import Red from './Red.jsx';    // Ensure this import points to the correct file location
 
+function App() {
+  return (
+    <>
+      <div id="container">
+        <h1>Your App</h1>
+        <div id="navbar">
+          <ul>
+            <li><Link to="/blue">Blue</Link></li>
+            <li><Link to="/red">Red</Link></li>
+          </ul>
+        </div>
+
+        <div id="main-section">
+          <Routes>
+            <Route path="/blue" element={<Blue />} />
+            <Route path="/red" element={<Red />} />
+          </Routes>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
